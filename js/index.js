91e364c2710ec7e3d393194b671a765cb3fd7c01
Gustavo Escoto <gustavo.escoto@dbccompany.com.br>
2023-03-06 01:43:16 -0300

    organizando por hora

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 const regex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/g;
 let tasksUl
-
+var divHora;
+const reminderList = document.getElementById('reminder-list');
 
 function addTask() {
-	const reminderList = document.getElementById('reminder-list');
 	let hour;
 	while (!regex.test(hour)) {
 		hour = prompt('Digite a hora escolhida! (hh:mm)');
@@ -16,9 +16,10 @@ function createLi(hour, message) {
 	const createItem = document.createElement('li');
 	createItem.classList.add('reminder-item');
 	createItem.classList.add(`date-${date.getFullYear()}${date.getMonth()}`);
+	createItem.classList.add(`hour-${hour}`)
 	createItem.innerHTML = `
   <div>${message}</div>
-  <div>${hour}</div>
+  <div id="hora">${hour}</div>
  `;
 	createItem.classList;
 	return createItem;
@@ -28,16 +29,31 @@ const date = new Date();
 
 function updateDate() {
 	document.getElementById("display-date").innerHTML = date.toLocaleDateString('pt-BR', { month: 'short', year: 'numeric' });
-
 	tasksUl = document.querySelectorAll("#reminder-list li");
-    tasksUl = ordenaPorHora(Array.from(tasksUl))
-	for (let t = 0; t < tasksUl.length; t++){
-		if(tasksUl[t].classList.contains(`date-${date.getFullYear()}${date.getMonth()}`)) {
-            document.getElementById('hidden').style.display = 'none'
+	divHora = document.querySelectorAll("#hora");
+	console.log(tasksUl);
+
+	for (let t = 0; t < tasksUl.length; t++) {
+		if (tasksUl[t].classList.contains(`date-${date.getFullYear()}${date.getMonth()}`)) {
+			(() => {
+				const frag = document.createDocumentFragment();
+				const list = document.querySelector("ul");
+				const items = list.querySelectorAll("li");
+				const sortedList = Array.from(items).sort(function (a, b) {
+					const c = a.lastElementChild.textContent,
+						d = b.lastElementChild.textContent;
+					return c < d ? -1 : c > d ? 1 : 0;
+				});
+				for (let item of sortedList) {
+					frag.appendChild(item);
+				}
+				list.appendChild(frag);
+			})();
+			document.getElementById('hidden').style.display = 'none'
 			tasksUl[t].style.display = "flex";
 		} else {
 			tasksUl[t].style.display = "none";
-            document.getElementById('hidden').style.display = 'block'
+			document.getElementById('hidden').style.display = 'flex'
 		}
 	}
 
@@ -55,7 +71,16 @@ function minusMonthInDate() {
 	updateDate();
 }
 
-function ordenaPorHora(arr){
-    let tasksOrdenadas = arr.sort((a,b)=>a-b)
-    return tasksOrdenadas
+function ordenaPorHora(arr) {
+	let ordenaTask = arr.sort((a, b) => {
+		let horaA = a.lastElementChild.textContent;
+		let horaB = b.lastElementChild.textContent;
+		return horaA.localeCompare(horaB);
+	})
+	console.log(ordenaTask);
+	var ordenaTaskNodeList = document.createDocumentFragment();
+	ordenaTask.forEach(element => {
+		ordenaTaskNodeList.appendChild(element.cloneNode())
+	});
+	return ordenaTaskNodeList.childNodes;
 }
